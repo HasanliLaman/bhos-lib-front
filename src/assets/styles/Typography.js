edcf60264691103679b8596f0a815f36ba140e5e
“Laman <hesenlileman2001@gmail.com>
2023-02-09 12:14:14 +0400

    Create boilerplate

---
+++ src/assets/styles/Typography.js
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+
+const Typography = css`
+  body {
+    font-family: "Sora", sans-serif;
+    font-size: 1.4rem;
+    font-weight: 300;
+    color: var(--color-text);
+    line-height: 2.4rem;
+  }
+`;
+
+export default Typography;
---
+++ src/assets/styles/Variables.js
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+const Variables = css`
+  :root {
+    --color-white: #fff;
+    --color-primary: #f65d4e;
+    --color-primary-hover: #f4402f;
+    --color-text: #444444;
+    --color-accent: #000000;
+    --color-lighter: #999999;
+    --color-border: #e6e6e6;
+
+    --screen-sm: 576px;
+    --screen-md: 768px;
+    --screen-lg: 992px;
+    --screen-xl: 1200px;
+  }
+`;
+
+export default Variables;
